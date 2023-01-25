956537dec3477aa89fe8f50c0b1e065cf1bb1bda
Patrik <patrik.nuernberger@gmail.com>
2023-01-25 14:42:09 +0100

    Inital commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+/** @jsxImportSource @emotion/react */
+import GuestElement from './GuestElement.js';
+import { css } from '@emotion/react';
+
+function App() {
+  return (
+    <>
+      <header>header</header>
+      <main>
+        <GuestElement />
+      </main>
+      <footer>footer</footer>
+    </>
+  );
+}
+
+export default App;
