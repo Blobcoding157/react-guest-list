9f603c310f4f0a48bcc7e16e3b3a6be692a04676
Patrik <patrik.nuernberger@gmail.com>
2023-01-27 16:01:42 +0100

    Implement css styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 /** @jsxImportSource @emotion/react */
 import GuestElement from './GuestElement.js';
+import { css } from '@emotion/react';
 
-// import { css } from '@emotion/react';
-
+const container = css`
+  border: double;
+  min-block-size: 600px;
+  border-color: tomato;
+  background-color: gainsboro;
+  align-items: flex-start;
+  display: flex;
+  padding: 20px;
+  flex-direction: row;
+`;
 function App() {
   return (
     <>
-      <header>header</header>
-      <main>
+      <main css={container}>
         <GuestElement />
       </main>
-      <footer>footer</footer>
+      <footer>©made by Patrik Industries wow!</footer>
     </>
   );
 }
--- src/GuestElement.js
+++ src/GuestElement.js
@@ -2,12 +2,100 @@
 import { css } from '@emotion/react';
 import { useEffect, useState } from 'react';
 
-const headerColor = css`
-  color: #ff0fff;
+const makeItdissapear = css`
+  font-size: 0px;
+  border-radius: 5px;
 `;
 
-// Base URL
-const baseUrl = 'http://localhost:4000';
+const inputContainer = css`
+  border: thin;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  background-color: whitesmoke;
+  box-shadow: 0 5px 12px 2px rgba(0, 0, 0, 0.15);
+  border-radius: 11px;
+`;
+
+const customerInput = css`
+  align-items: center;
+  display: grid;
+  margin: 8px;
+  border-radius: 5px;
+  padding: 5px 10px;
+  box-shadow: 0 5px 12px 2px rgba(0, 0, 0, 0.15);
+`;
+const button = css`
+  padding: 10px 25px;
+  font-size: 20px;
+  font-weight: 700;
+  border: none;
+  background-color: lightcoral;
+  border-radius: 11px;
+  cursor: pointer;
+  box-shadow: 0 5px 12px 2px rgba(0, 0, 0, 0.15);
+  transform: translate3d(0, -5px, 5px);
+
+  transition: color 0.5s ease-in-out, transform 0.3s ease-in-out,
+    box-shadow 0.3s ease-in-out;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover {
+    transform: translate3d(0, 0, 0);
+    box-shadow: none;
+  }
+`;
+const deleteButton = css`
+  // padding: 5px 5px;
+  font-size: 20px;
+  margin: 10px;
+  border: thin;
+  background-color: lightcoral;
+  border-radius: 11px;
+  cursor: pointer;
+  box-shadow: 0 5px 12px 2px rgba(0, 0, 0, 0.15);
+
+  transition: color 0.5s ease-in-out, transform 0.3s ease-in-out,
+    box-shadow 0.3s ease-in-out;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover {
+    border: solid;
+    border-color: black;
+  }
+`;
+
+const boxTick = css`
+  border-radius: 5px;
+  border: solid;
+`;
+
+const customerContainer = css`
+  border: thin;
+  background-color: whitesmoke;
+  margin: 30px 80px;
+  padding: 10px;
+  box-shadow: 0 5px 12px 2px rgba(0, 0, 0, 0.15);
+  border-radius: 11px;
+`;
+const customerList = css`
+  list-style: none;
+`;
+// Base URL physical Server
+
+// const baseUrl = 'http://localhost:4000';
+
+// Base URL with Replit
+
+const baseUrl =
+  'https://express-guest-list-api-memory-data-store-1.blobcoding157.repl.co';
 
 export default function GuestElement() {
   const [restaurantCustomers, setRestaurantCustomers] = useState([]);
@@ -88,10 +176,11 @@ export default function GuestElement() {
   }
   return (
     <>
-      <div>
-        <label css={headerColor}>
+      <div css={inputContainer}>
+        <label css={makeItdissapear}>
           First name
           <input
+            css={customerInput}
             disabled={isInputDisabled}
             value={first}
             placeholder="First Name"
@@ -100,9 +189,10 @@ export default function GuestElement() {
             }}
           />
         </label>
-        <label>
+        <label css={makeItdissapear}>
           Last name
           <input
+            css={customerInput}
             disabled={isInputDisabled}
             value={last}
             placeholder="Last Name"
@@ -120,6 +210,7 @@ export default function GuestElement() {
           />
         </label>
         <button
+          css={button}
           onClick={() => {
             deleteAll();
           }}
@@ -127,19 +218,29 @@ export default function GuestElement() {
           delete all
         </button>
       </div>
-      <div data-test-id="guest">
+      <div data-test-id="guest" css={customerContainer}>
         {isLoading ? <div>Loading...</div> : ''}
-        <ul>
+        <ul css={customerList}>
           {!isLoading &&
             restaurantCustomers.map((customer) => {
               return (
                 <li key={`Customer-${customer.id}`}>
+                  <button
+                    css={deleteButton}
+                    aria-label={`Remove ${customer.firstName} ${customer.lastName}`}
+                    onClick={async () => {
+                      await deleteGuest(customer.id);
+                    }}
+                  >
+                    X
+                  </button>
                   <span>
                     {customer.firstName} {customer.lastName}
                   </span>
 
                   {customer.attending ? ' is Attending!' : ''}
                   <input
+                    css={boxTick}
                     aria-label="attending"
                     type="checkbox"
                     checked={customer.attending}
@@ -149,14 +250,6 @@ export default function GuestElement() {
                       await updateGuest(customer.id);
                     }}
                   />
-                  <button
-                    aria-label={`Remove ${customer.firstName} ${customer.lastName}`}
-                    onClick={async () => {
-                      await deleteGuest(customer.id);
-                    }}
-                  >
-                    x
-                  </button>
                 </li>
               );
             })}
